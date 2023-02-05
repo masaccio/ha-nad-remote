2815523ac9a5e6f40f75cf5ea3c98ab393325731
Jon Connell <python@figsandfudge.com>
2023-02-05 17:46:48 +0000

    Sources support

--- custom_components/nad_remote/api.py
+++ custom_components/nad_remote/api.py
@@ -1,47 +1,75 @@
-"""Sample API Client."""
+"""NAD Amplifiler API Client for Home Assistant"""
 import logging
 import re
-from nad_receiver import NADReceiverTelnet
-
-
-API_TIMEOUT = 10
-DISCOVERY_TIMEOUT = 5
-
+import sys
 
 _LOGGER: logging.Logger = logging.getLogger(__package__)
+sys.path = ["/config/custom_components/nad_remote"] + sys.path
 
-HEADERS = {"Content-type": "application/json; charset=UTF-8"}
+# Use local implementation of NAD client rather than upstream
+from .nad_receiver import NADReceiverTelnet
+from .const import DOMAIN
+
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
 
 
 class NADApiClient:
     def __init__(self, host: str, port: int) -> None:
-        """Sample API Client."""
-        _LOGGER.debug("API init: host=%s, port=%s", host, port)
+        """NAD API Client."""
         self._host = host
         self._port = port
-        self._receiver = NADReceiverTelnet(host, port)
+        self.capabilities = None
+        self.receiver = NADReceiverTelnet(host, port)
 
-    async def async_get_model(self):
+    def get_model(self):
         try:
-            _LOGGER.debug("API get model: host=%s, port=%s", self._host, self._port)
-            response = self._receiver.exec_command("main", "model", "?")
+            response = self.receiver.main_model("?")
             if not re.match(r"^\w+\d+", response):
-                _LOGGER.debug("NAD model not recognised model=%s", response)
+                _LOGGER.debug("Amplifier model '%s' not recognised", response)
                 return None
             else:
-                _LOGGER.debug("API get model: model=%s", response)
+                _LOGGER.debug("Amplifier model='%s'", response)
                 return response
         except Exception as e:
-            _LOGGER.error("Timeout error in auto-discovery - %s", e)
+            _LOGGER.error("Error checking amplifier model: %s", e)
 
-    async def async_get_capabilities(self):
+    def get_capabilities(self):
+        """Fetch status of amplifier to get capabilities"""
+        if self.capabilities is not None:
+            return self.capabilities
         try:
-            _LOGGER.debug("API get capabilities")
-            response = self._receiver.transport.communicate("?")
-            return response
+            self.capabilities = self.receiver.status_all()
+            if self.capabilities is None:
+                _LOGGER.error("Error fetching amplifier capabilities: no reuslts")
+            return self.capabilities
         except Exception as e:
-            _LOGGER.error("Error fetching amplifier capabilities- %s", e)
+            _LOGGER.error("Error fetching amplifier capabilities: %s", e)
 
-    async def async_get_data(self):
-        _LOGGER.debug("async_get_data")
-        return True
+    def get_sources(self):
+        """Discover list of sources"""
+        sources = {}
+        try:
+            capabilities = self.get_capabilities()
+            source_list = []
+            for source_id in range(1, 20):
+                s_en = f"source{source_id}_enabled"
+                s_name = f"source{source_id}_name"
+                if s_en in self.capabilities:
+                    if self.capabilities[s_en] == "No":
+                        sources[source_id] = None
+                    else:
+                        sources[source_id] = self.capabilities[s_name]
+                        source_list.append(self.capabilities[s_name])
+            _LOGGER.debug("Sources discovered: %s", ",".join(source_list))
+        except Exception as exception:
+            _LOGGER.debug("Error fetching capabilities: %s", exception)
+            raise UpdateFailed() from exception
+        return sources
+
+    @property
+    def has_zone2(self):
+        try:
+            _ = self.receiver.zone2_source("?")
+            return True
+        except ValueError:
+            return False
