990d4d2ff875f137eeb3e8229f3be93bdba97bf5
Jon Connell <python@figsandfudge.com>
2023-02-05 17:47:48 +0000

    Local NAD client

--- local/deploy.sh
+++ local/deploy.sh
@@ -1,19 +1,27 @@
 #! /bin/bash
 REMOTE_HOST="hass"
-SRCDIR="custom_components/nad_remote"
 
-echo $(tput setaf 2)Creating remote /config/$SRCDIR$(tput sgr0)
-ssh $REMOTE_HOST "mkdir -p /config/$SRCDIR"
-ssh $REMOTE_HOST "mkdir -p /config/$SRCDIR/translations"
+directories="custom_components/nad_remote/nad_receiver custom_components/nad_remote/translations"
 
-for file in $SRCDIR/*.py $SRCDIR/manifest.json; do
-  echo $(tput setaf 2)Copying $file$(tput sgr0)
-  scp -q $file $REMOTE_HOST:/config/$SRCDIR
+echo $(tput setaf 2)rmdir /config/custom_components/nad_remote$(tput sgr0)
+ssh $REMOTE_HOST "rm -rf /config/custom_components/nad_remote"
+
+for dir in $directories; do
+  echo $(tput setaf 2)mkdir /config/$dir$(tput sgr0)
+  ssh $REMOTE_HOST "mkdir -p /config/$dir"
 done
-for file in $SRCDIR/translations/*.json; do
-  echo $(tput setaf 2)Copying $file$(tput sgr0)
-  scp -q $file $REMOTE_HOST:/config/$SRCDIR/translations
+
+set -o noglob
+files="*.py *.json translations/*.json nad_receiver/*.py"
+for file in $files; do
+  dir="custom_components/nad_remote/"$(dirname "$file")
+  dir="${dir/\/./}"
+  file=$(basename "$file")
+  set +o noglob
+  echo $(tput setaf 2)copy "$dir/$file"$(tput sgr0)
+  scp -q $dir/$file $REMOTE_HOST:/config/$dir
 done
+
 echo $(tput setaf 2)"Restarting HA Core"$(tput sgr0)
 ssh $REMOTE_HOST "source /etc/profile.d/homeassistant.sh && ha core restart"
 echo $(tput setaf 2)"Done"$(tput sgr0)
