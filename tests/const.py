f978b424f427b8aa20e584e44aac37b224c842c3
Jon Connell <python@figsandfudge.com>
2023-03-05 17:47:08 +0000

    Initial API test support

--- tests/const.py
+++ tests/const.py
@@ -1,6 +1,19 @@
 """Constants for NAD Amplifer remote control tests."""
-from custom_components.nad_remote.const import (
-    CONF_IP_ADDRESS,
-)
+from homeassistant.const import CONF_HOST, CONF_NAME, CONF_PORT
 
-MOCK_CONFIG = {CONF_IP_ADDRESS: "test_ip"}
+MOCK_HOSTNAME = "test_hostname"
+MOCK_CONFIG = {CONF_HOST: MOCK_HOSTNAME, CONF_PORT: 9023, CONF_NAME: "NAD Test"}
+
+MOCK_STATUS_ALL = {
+    "source1_name": "Test Source 1",
+    "source1_enabled": "Yes",
+    "source2_name": "Test Source 2",
+    "source2_enabled": "Yes",
+    "source3_name": "Test Source 3",
+    "source3_enabled": "No",
+    "main_volume_min": "-90",
+    "main_volume_max": "5",
+    "zone2_volume_min": "-100",
+    "zone2_volume_max": "10",
+}
+MOCK_MODEL = "T1234"
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,69 +1,93 @@
-"""Tests for NAD Amplifer remote control api."""
+"""Tests for NAD Home Assistant API."""
+
 import asyncio
+import pytest
 
-import aiohttp
-from custom_components.nad_remote.api import (
-    NADApiClient,
+from datetime import timezone
+from unittest.mock import MagicMock, patch
+
+from custom_components.nad_remote.api import NADApiClient
+from .const import MOCK_HOSTNAME, MOCK_MODEL
+
+
+from .const import MOCK_MODEL, MOCK_STATUS_ALL
+
+MOCK_RX_STATE = {
+    "main_mute": "Off",
+    "main_power": "Off",
+    "main_volume": -30,
+    "zone2_mute": "Off",
+    "zone2_power": "Off",
+    "zone2_volume": -30,
+    "main_source": "Test Source 1",
+    "zone2_source": "Test Source 1",
+    "main_listeningmode": "Stereo",
+}
+
+
+def mock_rx_func(name: str, op: str, arg: str):
+    if op == "?":
+        return MOCK_RX_STATE[name]
+    else:
+        MOCK_RX_STATE[name] = arg
+
+
+def mock_main_mute(op: str, arg: str = None):
+    return mock_rx_func("main_mute", op, arg)
+
+
+def mock_zone2_mute(op: str, arg: str = None):
+    return mock_rx_func("zone2_mute", op, arg)
+
+
+def mock_main_power(op: str, arg: str = None):
+    return mock_rx_func("main_power", op, arg)
+
+
+def mock_zone2_power(op: str, arg: str = None):
+    return mock_rx_func("zone2_power", op, arg)
+
+
+def mock_main_source(op: str, arg: str = None):
+    return mock_rx_func("main_source", op, arg)
+
+
+def mock_main_volume(op: str, arg: str = None):
+    return mock_rx_func("main_volume", op, arg)
+
+
+def mock_main_listeningmode(op: str, arg: str = None):
+    return mock_rx_func("main_listeningmode", op, arg)
+
+
+def mock_zone2_source(op: str, arg: str = None):
+    return mock_rx_func("zone2_source", op, arg)
+
+
+def mock_zone2_volume(op: str, arg: str = None):
+    return mock_rx_func("zone2_volume", op, arg)
+
+
+# with patch("custom_components.nad_remote.nad_receiver.NADReceiverTelnet") as mock_rx:
+# with patch("custom_components.nad_remote.api.NADApiClient.NADReceiverTelnet") as mock_rx:
+
+
+@pytest.mark.asyncio
+@patch.multiple(
+    "custom_components.nad_remote.nad_receiver.NADReceiverTelnet",
+    main_model=MagicMock(return_value=MOCK_MODEL),
+    status_all=MagicMock(return_value=MOCK_STATUS_ALL),
+    main_mute=MagicMock(side_effect=mock_main_mute),
+    main_power=MagicMock(side_effect=mock_main_power),
+    main_source=MagicMock(side_effect=mock_main_source),
+    main_volume=MagicMock(side_effect=mock_main_volume),
+    main_listeningmode=MagicMock(side_effect=mock_main_listeningmode),
+    zone2_mute=MagicMock(side_effect=mock_zone2_mute),
+    zone2_power=MagicMock(side_effect=mock_zone2_power),
+    zone2_source=MagicMock(side_effect=mock_zone2_source),
+    zone2_volume=MagicMock(side_effect=mock_zone2_volume),
 )
-from homeassistant.helpers.aiohttp_client import async_get_clientsession
-
-
-async def test_api(hass, aioclient_mock, caplog):
+async def test_api(hass):
     """Test API calls."""
-
-    # To test the api submodule, we first create an instance of our API client
-    api = NADApiClient("test", "test", async_get_clientsession(hass))
-
-    # Use aioclient_mock which is provided by `pytest_homeassistant_custom_components`
-    # to mock responses to aiohttp requests. In this case we are telling the mock to
-    # return {"test": "test"} when a `GET` call is made to the specified URL. We then
-    # call `async_get_data` which will make that `GET` request.
-    aioclient_mock.get("https://jsonplaceholder.typicode.com/posts/1", json={"test": "test"})
-    assert await api.async_get_data() == {"test": "test"}
-
-    # We do the same for `async_set_title`. Note the difference in the mock call
-    # between the previous step and this one. We use `patch` here instead of `get`
-    # because we know that `async_set_title` calls `api_wrapper` with `patch` as the
-    # first parameter
-    aioclient_mock.patch("https://jsonplaceholder.typicode.com/posts/1")
-    assert await api.async_set_title("test") is None
-
-    # In order to get 100% coverage, we need to test `api_wrapper` to test the code
-    # that isn't already called by `async_get_data` and `async_set_title`. Because the
-    # only logic that lives inside `api_wrapper` that is not being handled by a third
-    # party library (aiohttp) is the exception handling, we also want to simulate
-    # raising the exceptions to ensure that the function handles them as expected.
-    # The caplog fixture allows access to log messages in tests. This is particularly
-    # useful during exception handling testing since often the only action as part of
-    # exception handling is a logging statement
-    caplog.clear()
-    aioclient_mock.put("https://jsonplaceholder.typicode.com/posts/1", exc=asyncio.TimeoutError)
-    assert await api.api_wrapper("put", "https://jsonplaceholder.typicode.com/posts/1") is None
-    assert (
-        len(caplog.record_tuples) == 1
-        and "Timeout error fetching information from" in caplog.record_tuples[0][2]
-    )
-
-    caplog.clear()
-    aioclient_mock.post("https://jsonplaceholder.typicode.com/posts/1", exc=aiohttp.ClientError)
-    assert await api.api_wrapper("post", "https://jsonplaceholder.typicode.com/posts/1") is None
-    assert (
-        len(caplog.record_tuples) == 1
-        and "Error fetching information from" in caplog.record_tuples[0][2]
-    )
-
-    caplog.clear()
-    aioclient_mock.post("https://jsonplaceholder.typicode.com/posts/2", exc=Exception)
-    assert await api.api_wrapper("post", "https://jsonplaceholder.typicode.com/posts/2") is None
-    assert (
-        len(caplog.record_tuples) == 1
-        and "Something really wrong happened!" in caplog.record_tuples[0][2]
-    )
-
-    caplog.clear()
-    aioclient_mock.post("https://jsonplaceholder.typicode.com/posts/3", exc=TypeError)
-    assert await api.api_wrapper("post", "https://jsonplaceholder.typicode.com/posts/3") is None
-    assert (
-        len(caplog.record_tuples) == 1
-        and "Error parsing information from" in caplog.record_tuples[0][2]
-    )
+    api = NADApiClient(MOCK_HOSTNAME, 23)
+    assert api.get_model() == MOCK_MODEL
